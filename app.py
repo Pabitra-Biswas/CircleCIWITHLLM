1b27680aab59884b097a34a0d4725b210d1dd531
Pabitra-Biswas <159247582+Pabitra-Biswas@users.noreply.github.com>
2025-09-30 10:35:53 +0530

    Trigger CI evaluation pipeline

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from langchain.prompts import ChatPromptTemplate
-from langchain.chat_models import ChatOpenAI
 from langchain.schema.output_parser import StrOutputParser
 from langchain_google_genai import ChatGoogleGenerativeAI
 delimiter = "####"
